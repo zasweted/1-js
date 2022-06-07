3d0f48ac61823579e5b28a496aa8f4328a13e2e9
Aleksandr Vasiljev <zaswet@gmail.com>
2022-06-07 12:31:52 +0300

    kintamieji

---
+++ index.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/*
+kintamuju inicijavimo budai:
+const - kai verte negali keistis (default)
+let - kai verte norime, jog galetu keistis
+var - senasis budas (nenaudoti, jei nenori PROBLEMU)
+*/
+
+const username = 'Petras';
+
+let pinigine = 0;
+console.log(username, 'pinigineje turi', pinigine, 'pinigu');
+
+pinigine = pinigine + 1;
+console.log(username, 'pinigineje turi', pinigine, 'pinigu');
+
+pinigine = pinigine + 1;
+console.log(username, 'pinigineje turi', pinigine, 'pinigu');
