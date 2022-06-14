9c1434e855e5caa90ae7fe15bf5334cf553bc34d
Aleksandr Vasiljev <zaswet@gmail.com>
2022-06-14 11:31:57 +0300

    for

--- js/for.js
+++ js/for.js
@@ -42,3 +42,42 @@ for (let i = 0; i < word.length; i++) {
     const letter = word[i];
     console.log(i, ')', letter);
 }
+
+console.clear();
+
+for (let i = 10; i > 0; i--) {
+    console.log(i);
+    if (i === 5) {
+        break;
+    }
+
+} 
+console.log('----------------------');
+console.log('-------- min money');
+const money = [5, 10, 20, 50, 100, 5, 10, 20];
+const minTarget = 80;
+const hand = [];
+let handTotal = 0;
+
+for (let i = 0; i < money.length; i++) {
+    const value = money[i];
+    handTotal += value;
+    hand.push(value);
+
+    console.log(`${i}) ${value} -> total: ${handTotal};`, hand);
+
+    if (handTotal >= minTarget) {
+        break;
+    }
+}
+
+const graza = handTotal - minTarget;
+if (graza > 0) {
+    console.log(`Graza: ${graza}`);
+    console.log('Duotos kupiuros:', hand);
+} else if (graza === 0) {
+    console.log('Duota lygiai tiek kiek reikia pinigu');
+    console.log('Duotos kupiuros:', hand);
+} else {
+    console.log(`Tu skoloje dar ${graza * - 1} pinigu`);
+}
