020d6ef6df7be7afd96e7ab220de848603f2bdf1
Aleksandr Vasiljev <zaswet@gmail.com>
2022-06-16 11:49:16 +0300

    truphy falsy values

---
+++ js/if-values.js
@@ -0,0 +1,34 @@
+console.clear();
+/*
+REIKSMIU POZITIVUMAS/ NEGATYVUMAS
+skaiciai:
+    0 - negativus
+    kiti - pozitivus
+boolean:
+    true - pozitivus
+    false - negativus
+
+    
+string:
+    ne tuscias - pozitivus
+    tuscias - negativus
+
+array:
+    visada pozitivus
+
+object:
+    visada pozitivus
+
+function:
+    visada pozitivus
+
+*/
+
+const userInput = ' ';
+
+if (userInput) {
+    console.log('taip');
+} else {
+    console.log('ne');
+}
+
