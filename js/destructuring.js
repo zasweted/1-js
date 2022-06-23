8cf71d8498dd8c4fe16e9a827dc3a05697e1a3bd
Aleksandr Vasiljev <zaswet@gmail.com>
2022-06-23 12:25:42 +0300

    destruct

--- js/destructuring.js
+++ js/destructuring.js
@@ -47,3 +47,43 @@ const [first, second, ...other] = marks;
 console.log(first);
 console.log(second);
 console.log(other);
+
+function info(...params){
+    return `info: ${params.length}`;
+}
+
+console.log(info());
+console.log(info(undefined));
+console.log(info(1));
+console.log(info(1, undefined));
+console.log(info(1, 2));
+console.log(info(1, 2, 'labas'));
+console.log(info(1, 2, 'labas', true));
+
+console.log('#####################');
+// function biggest(a, b){
+//     if (a > b) {
+//         return a;
+//     }
+//     return b;
+// }
+
+function biggest(...list){
+    return Math.max(...list);
+}
+
+console.log(biggest(2, 4));
+console.log(biggest(22, 4));
+console.log(biggest(7));
+console.log(biggest(7, 8, -4, 9)); 
+
+function asd(firstName, lastName, ...other) {
+    console.log(firstName);
+    console.log(lastName);
+    console.log(otherStuff);
+    return ' --------------------- '
+}
+
+console.log(example('Petras',));
+console.log(example('Petras', 'Petraitis'));
+console.log(example('Petras', 'Petraitis', 1, 2, 3, 4));
